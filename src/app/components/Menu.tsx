3497a961362d43aa3109b9bacd297ae1d05a279c
wojiaomeli <wojiaomelissa@gmail.com>
2024-07-19 10:49:31 +0200

    menu adapté

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -21,10 +21,10 @@ const Menu = ({ searchVisible, setSearchVisible }) => {
 
   const menuItems = [
     { id: 'ressources', label: 'Toutes les ressources', link: '/ressource', icon: null, style: 'btnBluePermanent' },
-    { id: 'methodologie', label: 'Méthodologie', link: '/methodologie', icon: <FiSettings className="text-redF text-xl mr-1" />, style: 'btnWhite' },
-    { id: 'promotion', label: 'Promotion', link: '/promotion', icon: <BsMegaphone className="text-greenF text-xl mr-1" />, style: 'btnWhite' },
-    { id: 'classe', label: 'Pour la classe', link: '/classe', icon: <GrGroup className="text-yellowF text-xl mr-1" />, style: 'btnWhite' },
-    { id: 'pays', label: 'Pays', link: '/pays', icon: <IoMdGlobe className="text-purplF text-xl mr-1" />, style: 'btnWhite' },
+    { id: 'methodologie', label: 'Méthodologie', link: '/methodologie', icon: <FiSettings className="text-redF text-lg mr-1" /> },
+    { id: 'promotion', label: 'Promotion', link: '/promotion', icon: <BsMegaphone className="text-greenF text-lg mr-1" /> },
+    { id: 'classe', label: 'Pour la classe', link: '/classe', icon: <GrGroup className="text-yellowF text-lg mr-1" /> },
+    { id: 'pays', label: 'Pays', link: '/pays', icon: <IoMdGlobe className="text-purplF text-lg mr-1" /> },
   ];
 
   return (
@@ -32,34 +32,32 @@ const Menu = ({ searchVisible, setSearchVisible }) => {
       <div className="max-w-screen-xl mx-auto px-4 py-2 flex items-center justify-between">
         {/* Logo */}
         <div className="flex-shrink-0 mr-4">
-          <Link href="/" passHref legacyBehavior>
-            <a className="block">
-              <Image
-                src="/assets/logo fil.svg" // Vérifiez le chemin de l'image
-                alt="Logo"
-                width={180}  // Ajustez la largeur du logo
-                height={60}  // Ajustez la hauteur du logo
-              />
-            </a>
+          <Link href="/" passHref>
+            <Image
+              src="/assets/logo fil.svg" // Vérifiez le chemin de l'image
+              alt="Logo"
+              width={160}  // Ajustez la largeur du logo
+              height={50}  // Ajustez la hauteur du logo
+            />
           </Link>
         </div>
 
         {/* Menu */}
-        <div className="flex-grow flex justify-center">
+        <div className="flex-grow flex items-center justify-center mx-4">
           {!searchVisible && (
-            <ul className="flex items-center gap-4 uppercase text-sm font-medium flex-wrap">
+            <ul className="flex items-center gap-6 uppercase text-xs font-medium">
               {menuItems.map(item => (
                 <li
                   key={item.id}
-                  className="cursor-pointer duration-300 menuItem"
+                  className={`cursor-pointer duration-300 flex items-center ${item.id !== 'ressources' ? (hoveredItem === item.id ? 'bg-blue-600 text-white' : 'bg-white text-black') : ''} ${item.id !== 'ressources' ? 'px-3 py-1 rounded-md' : styles[item.style]}`}
                   onMouseEnter={() => handleMouseEnter(item.id)}
                   onMouseLeave={handleMouseLeave}
                 >
-                  <Link href={item.link} passHref legacyBehavior>
-                    <a className={`flex items-center ${styles[item.style]} ${item.id !== 'ressources' && hoveredItem === item.id ? styles.btnBlue : ''}`}>
+                  <Link href={item.link} passHref>
+                    <div className="flex items-center">
                       {item.icon}
                       <span className={`ml-1 ${item.id === 'pays' ? 'ml-2' : ''}`}>{item.label}</span>
-                    </a>
+                    </div>
                   </Link>
                 </li>
               ))}
@@ -68,9 +66,9 @@ const Menu = ({ searchVisible, setSearchVisible }) => {
         </div>
 
         {/* Recherche et autres éléments */}
-        <div className="flex gap-4 items-center">
+        <div className="flex gap-4 items-center ml-4">
           <BsSearch
-            className="text-xl hover:text-gray-600 cursor-pointer"
+            className="text-lg hover:text-gray-600 cursor-pointer"
             onClick={() => setSearchVisible(!searchVisible)}
           />
           {searchVisible && (
@@ -80,7 +78,9 @@ const Menu = ({ searchVisible, setSearchVisible }) => {
               className="w-32 sm:w-40 h-8 bg-white text-black uppercase text-sm font-normal rounded-md border-gray-300 px-2"
             />
           )}
-          <ScientificCommitteeButton />
+          <div className="ml-4">
+            <ScientificCommitteeButton />
+          </div>
         </div>
       </div>
     </div>
@@ -88,9 +88,7 @@ const Menu = ({ searchVisible, setSearchVisible }) => {
 };
 
 const styles = {
-  btnBluePermanent: 'px-2 py-1 text-white bg-blue-600 rounded-md transition duration-300 ease-in-out text-sm',
-  btnWhite: 'px-2 py-1 text-black bg-white rounded-md transition duration-300 ease-in-out text-xs',
-  btnBlue: 'bg-blue-500 text-white'
+  btnBluePermanent: 'px-4 py-2 text-white bg-blue-600 rounded-md transition duration-300 ease-in-out text-sm',
 };
 
 export default Menu;
